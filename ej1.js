e7102c4aaf049ee507034a5dba3d57cde666379d
Nicolas <d51702027@alumnos.itr.edu.ar>
2023-09-07 20:25:09 -0300

    s

--- ej1.js
+++ ej1.js
@@ -1,18 +1,16 @@
-function recursiveBubbleSort(arr, n, i) {
-  if (n === 1 || i === n - 1) {
-    return;
+function recursiveBubbleSort(arr, n) {
+  if (n === 1) {
+    return arr;
   }
-  if (arr[i] > arr[i + 1]) {
-    [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
-  }
-  recursiveBubbleSort(arr, n, i + 1);
 
-  if (i === 0) {
-    recursiveBubbleSort(arr, n - 1, 0);
+  for (let i = 0; i < n - 1; i++) {
+    if (arr[i] > arr[i + 1]) {
+      [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+    }
   }
+
+  return recursiveBubbleSort(arr, n - 1);
 }
 
-let i = 0;
 let arr = [64, 34, 25, 12, 22, 11, 90];
-recursiveBubbleSort(arr, arr.length, i);
-console.log(arr);
+console.log(recursiveBubbleSort(arr, arr.length));
